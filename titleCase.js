d5dcf6e86681a7f7bf557b52e6c535b73fd511e9
purdyjen <purdyjen@me.com>
2022-11-21 12:12:53 -0500

    cleaned up comments in titlecase

--- titleCase.js
+++ titleCase.js
@@ -1,19 +1,20 @@
+/*
+Parameters, Return, Example, Pseudocode (PREP)
+P: Input of a string
+R: Outputs the same string, but with title casing
+E: "I'm a little tea pot" / "I'M A LITTLE TEA POT"
+P: 1. lowercase everything
+P: 2. split the string into separate words
+P: 3. for loop
+     - iterate through each word
+     - access the first character (0 index) and save to variable (firstChar)
+     - slice (copy) any characters at index 1 and later (assuming any exist) (wordRemainder)
+     - uppercase firstChar
+     - join firstChar and wordRemainder at str[i]
+P: 4. Join elements in str array with spaces
+P: 5. return str
+*/
 function titleCase(str) {
-  // Parameters, Return, Example, Pseudocode (PREP)
-  // P: Input of a string
-  // R: Outputs the same string, but with title casing
-  // E: "I'm a little tea pot" / "I'M A LITTLE TEA POT"
-  // P: 1. lowercase everything
-  // P: 2. split the string into separate words
-  // P: 3. for loop
-  //       - iterate through each word
-  //       - access the first character (0 index) and save to variable (firstChar)
-  //       - slice (copy) any characters at index 1 and later (assuming any exist) (wordRemainder)
-  //       - uppercase firstChar
-  //       - join firstChar and wordRemainder at str[i]
-  // P: 4. Join elements in str array with spaces
-  // P: 5. return str
-
   str = str.toLowerCase();
 
   str = str.split(" ");
